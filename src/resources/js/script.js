e3689d6862654f3809dd6dcaa6e31dff976402b8
egy186 <egy186@gmail.com>
2024-12-26 23:46:40 +0900

    fix: no need to wait load of highlight.js

--- src/resources/js/script.js
+++ src/resources/js/script.js
@@ -6,30 +6,19 @@ const extname = path => {
   return dot === 0 ? '' : path.substr(dot).toLowerCase();
 };
 
-const highlight = () => {
-  const languages = hljs.listLanguages().reduce((arr, language) => {
-    const { aliases = [] } = hljs.getLanguage(language);
-    return arr.concat(language, aliases);
-  }, []);
+const languages = hljs.listLanguages().reduce((arr, language) => {
+  const { aliases = [] } = hljs.getLanguage(language);
+  return arr.concat(language, aliases);
+}, []);
 
-  const org = document.querySelector('pre');
-  const pre = org.cloneNode(true);
-  const ext = extname(location.pathname);
-  if (languages.includes(ext)) {
-    pre.classList.add(`lang-${ext}`);
-  }
-
-  hljs.highlightElement(pre);
-  org.parentNode.replaceChild(pre, org);
-  // Background color
-  document.body.style.backgroundColor = getComputedStyle(pre).backgroundColor;
-};
-
-if (typeof hljs === 'undefined') {
-  // Wait hljs
-  document.querySelector('script').onload = () => {
-    highlight();
-  };
-} else {
-  highlight();
+const org = document.querySelector('pre');
+const pre = org.cloneNode(true);
+const ext = extname(location.pathname);
+if (languages.includes(ext)) {
+  pre.classList.add(`lang-${ext}`);
 }
+
+hljs.highlightElement(pre);
+org.parentNode.replaceChild(pre, org);
+// Background color
+document.body.style.backgroundColor = getComputedStyle(pre).backgroundColor;
